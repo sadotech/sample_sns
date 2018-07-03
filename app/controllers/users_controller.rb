9ec30051ecf40b2fe273fc1dd3d7a338c8a69672
Sadohara Hirokazu <sadohara.hirokazu@gmail.com>
2018-07-03 14:30:20 +0900

    ヘッダーのスタイル整えたり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(
+      account_id: user_params[:account_id], 
+      email: user_params[:email],
+      password: user_params[:password]
+    )
+
+    if user.save
+      redirect_to user
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -13,11 +24,18 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:account_id, :email, :password)
   end
 end
