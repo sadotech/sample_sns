110ee0b982c70780a4a9938ec7a22a7a59f6caa1
佐土原弘和 <hirokazu@SadoBookPro.local>
2018-08-09 10:00:58 +0900

    rilation、やや完成？

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -1,10 +1,2 @@
 class FollowRelationship < ActiveRecord::Base
-  create_table :relationships do |t|
-    t.integer :following_id
-    t.integer :followed_id
-    t.timestamps null: false
-  end
-  add_index :relationships, :following_id
-  add_index :relationships, :followed_id
-  add_index :relationships, [:following_id, :followed_id], unique: true
 end
